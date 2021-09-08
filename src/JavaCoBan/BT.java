1d54d973c4172f52217879faacab47dc0a89af52
Đạt <nguyentiendatnvnt@gmail.com>
2021-09-08 14:30:43 +0700

    first

---
+++ src/JavaCoBan/BT.java
@@ -0,0 +1,9 @@
+package JavaCoBan;
+public class BT {
+
+		public static void main(String[] args) {
+	        System.out.print("Hello world : ");
+	        System.out.println("Welcome to Java");
+
+		}
+}
